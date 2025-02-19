89f192f296a30d5d7ccd16bb3e3c97c0dc0d5c52
Daniel <izaguirredaniel9712@gmail.com>
2025-02-19 13:00:42 -0500

    Initial commit

---
+++ web-app/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+//const telegram = window.Telegram.WebApp;
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Telegram web app testing</h1>
+    </>
+  )
+}
+
+export default App
