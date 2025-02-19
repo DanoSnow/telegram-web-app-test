03041b5d18119d11e13eb75213f832ae19522f27
Daniel <izaguirredaniel9712@gmail.com>
2025-02-19 13:16:39 -0500

    Small fix

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-
 //const telegram = window.Telegram.WebApp;
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Telegram web app testing</h1>
