fc1ec836c63dfd6578ec625d92ac8aab1768fbbc
Daniel <izaguirredaniel9712@gmail.com>
2025-02-27 13:21:57 -0500

    Changing dialog dimensions to full screen

--- web-app/src/components/Map.tsx
+++ web-app/src/components/Map.tsx
@@ -7,6 +7,136 @@ import valley from '../assets/Valley.jpg'
 import village from '../assets/Village.jpg'
 import coliseum from '../assets/Coliseum.jpg'
 
+/*const locationsData = {
+    village: {
+        description: "A quaint village nestled between rolling hills, where cobblestone streets wind past charming cottages and the scent of fresh bread fills the air.",
+        tileImgUrl: village
+    },
+    coliseum: {
+        description: "An ancient coliseum, its weathered stones echoing the roars of past battles, stands as a testament to glory and valor, inviting brave souls to test their might.",
+        tileImgUrl: coliseum
+    },
+    goblinCave: {
+        description: "Dark and damp, the Goblin Cave is a labyrinth of twisting tunnels, filled with the sounds of chattering goblins and the glint of stolen treasures hidden in the shadows.",
+        tileImgUrl: goblinCave
+    },
+    desert: {
+        description: "A vast expanse of golden sands stretches endlessly under the scorching sun, where mirages dance on the horizon and secrets of the ancients lie buried beneath the dunes.",
+        tileImgUrl: desert
+    },
+    fairyForest: {
+        description: "A magical forest alive with shimmering lights and the sweet sound of laughter, where fairies flit between vibrant flowers and ancient trees whisper secrets of enchantment.",
+        tileImgUrl: fairyForest
+    },
+    forest: {
+        description: "A dense forest teeming with life, where towering trees create a canopy of green, and the air is filled with the sounds of rustling leaves and distant wildlife.",
+        tileImgUrl: forest
+    },
+    valley: {
+        description: "A serene valley cradled between majestic mountains, where a gentle river flows and wildflowers bloom in vibrant colors, creating a peaceful haven for travelers and creatures alike.",
+        tileImgUrl: forest
+    }
+}
+
+const mapData = [
+    {
+        cardinalDirection: "NW",
+        X: 2,
+        Y: 2,
+        location: locationsData.goblinCave
+    },
+    {
+        cardinalDirection: "NW",
+        X: 1,
+        Y: 2,
+        location: locationsData.desert
+    },
+    {
+        cardinalDirection: "NE",
+        X: 1,
+        Y: 2,
+        location: locationsData.valley
+    },
+    {
+        cardinalDirection: "NE",
+        X: 2,
+        Y: 2,
+        location: locationsData.forest
+    },
+    {
+        cardinalDirection: "NW",
+        X: 2,
+        Y: 1,
+        location: locationsData.forest
+    },
+    {
+        cardinalDirection: "NW",
+        X: 1,
+        Y: 1,
+        location: locationsData.village
+    },
+    {
+        cardinalDirection: "NE",
+        X: 1,
+        Y: 1,
+        location: locationsData.coliseum
+    },
+    {
+        cardinalDirection: "NE",
+        X: 2,
+        Y: 1,
+        location: locationsData.fairyForest
+    },
+    {
+        cardinalDirection: "SW",
+        X: 2,
+        Y: 1,
+        location: locationsData.fairyForest
+    },
+    {
+        cardinalDirection: "SW",
+        X: 1,
+        Y: 1,
+        location: locationsData.forest
+    },
+    {
+        cardinalDirection: "SE",
+        X: 1,
+        Y: 1,
+        location: locationsData.desert
+    },
+    {
+        cardinalDirection: "SE",
+        X: 2,
+        Y: 1,
+        location: locationsData.valley
+    },
+    {
+        cardinalDirection: "SW",
+        X: 2,
+        Y: 2,
+        location: locationsData.desert
+    },
+    {
+        cardinalDirection: "SW",
+        X: 1,
+        Y: 2,
+        location: locationsData.goblinCave
+    },
+    {
+        cardinalDirection: "SE",
+        X: 1,
+        Y: 2,
+        location: locationsData.valley
+    },
+    {
+        cardinalDirection: "SE",
+        X: 2,
+        Y: 2,
+        location: locationsData.forest
+    },
+]*/
+
 const Map = () => {
     return (
         <div className="space-y-1 p-1 flex flex-col items-center">
--- web-app/src/components/TileDetails.tsx
+++ web-app/src/components/TileDetails.tsx
@@ -1,4 +1,4 @@
-import { Button, Dialog, DialogPanel, DialogTitle, DialogBackdrop } from '@headlessui/react'
+import { Button, Dialog, DialogPanel, DialogTitle } from '@headlessui/react'
 import { useState, useEffect } from 'react'
 
 interface TileDetailsProps {
@@ -26,25 +26,30 @@ const TileDetails = ({ cardinalDirection, X, Y, tileImgUrl, isOpen, onClose }: T
   return (
     <>
       <Dialog open={tileDetailsState.isOpen} as="div" className="relative z-10 focus:outline-none" onClose={onClose}>
-        <DialogBackdrop className="fixed inset-0 bg-black/30" />
+        {/* <DialogBackdrop className="fixed inset-0 bg-black/30" /> */}
         <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
-          <div className="flex flex-col min-h-full items-center justify-center p-4">
+          <div className="flex flex-col min-h-full items-center justify-center rounded-none">
             <DialogPanel transition
-              className="w-full max-w-md rounded-xl bg-neutral-100 shadow-lg p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0 flex flex-col items-center"
+              className="w-screen h-screen rounded-xl bg-neutral-100 shadow-lg p-6 backdrop-blur-2xl duration-300 ease-out data-[closed]:transform-[scale(95%)] data-[closed]:opacity-0 flex flex-col items-center"
             >
-              <DialogTitle as="h3" className="text-black font-medium">
-                {`${tileDetailsState.cardinalDirection} (${tileDetailsState.X}, ${tileDetailsState.Y})`}
+              <DialogTitle as="h3" className="w-full text-black font-medium flex justify-between items-center">
+                <div className='w-full '>
+                  {`${tileDetailsState.cardinalDirection} (${tileDetailsState.X}, ${tileDetailsState.Y})`}
+                </div>
+                <div className='w-full flex flex-row-reverse'>
+                  <Button
+                    onClick={onClose}
+                    className="inline-flex items-center gap-2 rounded-full py-3 px-3 text-sm/6 font-semibold text-gray-500 shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-neutral-300 data-[open]:bg-gray-700 data-[focus]:outline-1 data-[focus]:outline-white cursor-pointer"
+                  >
+                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
+                    </svg>
+                    <span className="sr-only">Close modal</span>
+                  </Button>
+                </div>
               </DialogTitle>
               <div>
                 <img src={tileDetailsState.tileImgUrl} className="rounded-md" />
-              </div>
-              <div className="mt-4">
-                <Button
-                  className="inline-flex items-center gap-2 rounded-md bg-gray-700 py-1.5 px-3 text-sm/6 font-semibold text-white shadow-inner shadow-white/10 focus:outline-none data-[hover]:bg-gray-600 data-[focus]:outline-1 data-[focus]:outline-white data-[open]:bg-gray-700"
-                  onClick={onClose}
-                >
-                  Close Window
-                </Button>
               </div>
             </DialogPanel>
           </div>
